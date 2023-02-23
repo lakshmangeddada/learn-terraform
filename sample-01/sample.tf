32d777657643c3d69b35de7d5c883b6d4d32cbf3
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-23 12:20:01 +0530

    terraform

--- sample-01/sample.tf
+++ sample-01/sample.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
 
   tags = {
