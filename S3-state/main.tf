0d67cb00cfbb435d4195eb9afb79b1a50166466b
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-28 21:26:10 +0530

    updated

--- S3-state/main.tf
+++ S3-state/main.tf
@@ -1,15 +1,25 @@
-resource "aws_s3_bucket" "s3" {
-  bucket = "devops71-geddada"
-
-  tags = {
-    Name        = "geddada"
-    Environment = "Dev"
+terraform {
+  backend "s3" {
+    name = "s3-devops71"
+    key    = "S3-state/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
+variable "security_group" {
+  default   = [ "sg-0615d2d6b16a84caa" ]
+}
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.s3.id
-  acl = "private"
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "${var.security_group}"
+  tags = {
+    Name = "test"
   }
+}
 
+output "security_group_id" {
+  value = "${var.security_group}"
+}
