09aae497926900d3ca11f8fbc22baf0758db1cf0
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 23:51:55 +0530

    updated

--- sample-01/main.tf
+++ sample-01/main.tf
@@ -1,17 +1,9 @@
-variable "security_group" {
-  default   = [ "sg-0615d2d6b16a84caa" ]
-}
-
-
 resource "aws_instance" "ec2" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "${var.security_group}"
+  vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
   tags = {
     Name = "test"
   }
 }
 
-output "security_group_id" {
-  value = "${var.security_group}"
-}
