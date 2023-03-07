cc9deead25f9bfeb5e3384911079376da2e3bc7a
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-07 13:01:27 +0530

    updated

---
+++ roboshop-setup/ex2-loops-count/main.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ami" {
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+  most_recent = true
+}
+
+resource "aws_instance" "instances" {
+  count = length(var.instances)
+  ami = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0615d2d6b16a84caa"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+variable "instances" {
+  default = ["cart", "user", "mongodb"]
+}
+
+output "public_ips" {
+  value = aws_instance.instances.public_ip
+}
