751c39a0d12218b88dd10735e30062b8656cd0b7
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-17 22:14:50 +0530

    updated

--- practice-server/main.tf
+++ practice-server/main.tf
@@ -8,10 +8,10 @@ data "aws_ami" "ami" {
 resource "aws_instance" "instances" {
   count = length(var.instances)
   ami = data.aws_ami.ami.image_id
-  instance_type = "count.index[type]"
+  instance_type = count.index[type]
   vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
   tags = {
-    name = "count.index[name]"
+    name = count.index[name]
   }
 }
 
