2c83ca9c60f18043dfee6320f6bbb8b309e3b5ea
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-17 22:48:30 +0530

    updated

--- ec2-servers/servers.tf
+++ ec2-servers/servers.tf
@@ -13,3 +13,12 @@ resource "aws_instance" "prometheus" {
     Name = "prometheus"
   }
 }
+
+resource "aws_instance" "prometheus" {
+  ami = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
+  tags = {
+    Name = "node"
+  }
+}
