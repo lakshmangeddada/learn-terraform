f0550ad9e731b770621c48f4f1742f681bcf7f38
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-17 22:24:48 +0530

    updated

--- practice-server/main.tf
+++ practice-server/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "instances" {
   for_each = var.instances
   ami = data.aws_ami.ami.image_id
   instance_type = each.value["type"]
-  Name = each.value["name"]
+  component = each.value["name"]
   vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
 }
 
