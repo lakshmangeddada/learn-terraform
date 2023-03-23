6f418b0d1e302e1d895c99943aea98739d92b686
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-23 20:14:17 +0530

    updated

--- S3-state/main.tf
+++ S3-state/main.tf
@@ -1,28 +1,28 @@
 terraform {
   backend "s3" {
-    bucket = "geddada-devops71"
+    bucket = "devops-71"
     key    = "S3-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
-#variable "security_group" {
-#  default   = [ "sg-0615d2d6b16a84caa" ]
-#}
-#
-#
-#resource "aws_instance" "ec2" {
-#  ami           = "ami-0a017d8ceb274537d"
-#  instance_type = "t2.micro"
-#  vpc_security_group_ids = "${var.security_group}"
-#  tags = {
-#    Name = "test"
-#  }
-#}
-#
-#
-#output "security_group_id" {
-#  value = "${var.security_group}"
-#}
+variable "security_group" {
+  default   = [ "sg-0615d2d6b16a84caa" ]
+}
+
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "${var.security_group}"
+  tags = {
+    Name = "test"
+  }
+}
+
+
+output "security_group_id" {
+  value = "${var.security_group}"
+}
 
 
